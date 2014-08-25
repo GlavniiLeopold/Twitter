fb24904d45c7a81794f3e695d7b349034d5a3f72
Leopold <Leopoldglavnii@gmail.com>
2014-08-25 10:24:34 +0000

    That's all

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
-def new
+  def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-  if user && user.authenticate(params[:session][:password])
-    sign_in user
-    redirect_back_or user
-  else
-    flash.now[:error] = 'Упс!Неверный email или пароль'
-    render 'new'
-  end
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Упс!Неверный email или пароль'
+      render 'new'
+    end
   end
   
   def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,24 @@
 module SessionsHelper
    def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
-end
+      remember_token = User.new_remember_token
+      cookies.permanent[:remember_token] = remember_token
+      user.update_attribute(:remember_token, User.encrypt(remember_token))
+      self.current_user = user
+    end
    def signed_in?
-      !current_user.nil?
-end
+        !current_user.nil?
+    end
     def current_user=(user)
-    @current_user = user
-end
+      @current_user = user
+    end
   
     def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
-end
+      remember_token = User.encrypt(cookies[:remember_token])
+      @current_user ||= User.find_by(remember_token: remember_token)
+  end
   def current_user?(user)
-    user==current_user
-end
+      user==current_user
+  end
   def signed_in_user
     unless signed_in?
       store_location
@@ -30,12 +30,12 @@ end
                                             User.encrypt(User.new_remember_token))
         cookies.delete(:remember_token)
         self.current_user = nil
-end
+  end
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
     session.delete(:return_to)
-end
+  end
   def store_location
     session[:return_to] = request.url if request.get?
-end
+  end
 end
